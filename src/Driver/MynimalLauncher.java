72283ad8fc3184f242814b2d39ef4c2f4ffa306e
ronny macmaster <ronnymacmaster@gmail.com>
2016-05-15 02:09:37 -0500

    create user method

--- src/Driver/MynimalLauncher.java
+++ src/Driver/MynimalLauncher.java
@@ -7,17 +7,51 @@
  ************************************************************/
 package Driver;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * Top-level driver interface to launch the Mynimal RPG 
  */
-public class MynimalLauncher{
+public final class MynimalLauncher{
 
-	public MynimalLauncher(){
-
-	}
-
+	public static Set<String> playerSet = new HashSet<String>();
+	public static Map<String, String> passwordMap = new HashMap<String, String>();
+	
 	public static void main(String[] args){
 		System.out.println("Launching Mynimal, please wait...");
+		
+	}
+	
+	/**
+	 * Initialization routine
+	 */
+	public static void startRPG(){
+		
+	}
+	
+	/**
+	 * Creates a new Mynimal RPG user
+	 */
+	public static void createUser(){
+		// read a username
+		String username = "";
+		Scanner input = new Scanner(System.in);
+		
+		// poll valid username
+		System.out.println("Please choose a username: ");
+		username = input.nextLine().split(" ")[0];
+		while(playerSet.contains(username)){
+			System.out.println("Username taken! Please choose another username: ");
+			username = input.nextLine().split(" ")[0];
+		}
+		
+		// add new user
+		System.out.println("Welcome to MynimalRPG!");
+		playerSet.add(username);
 	}
 
 }
