07fd5c6cc4308cd88f9627935c89e96aa5855c81
ronny macmaster <ronnymacmaster@gmail.com>
2016-05-15 02:43:24 -0500

    create user functionality

--- src/Driver/MynimalLauncher.java
+++ src/Driver/MynimalLauncher.java
@@ -7,8 +7,12 @@
  ************************************************************/
 package Driver;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
@@ -19,39 +23,97 @@ import java.util.Set;
 public final class MynimalLauncher{
 
 	public static Set<String> playerSet = new HashSet<String>();
-	public static Map<String, String> passwordMap = new HashMap<String, String>();
+	public static Map<String, String> passwordMap = new LinkedHashMap<String, String>();
 	
 	public static void main(String[] args){
 		System.out.println("Launching Mynimal, please wait...");
+		openMynimal();
+		createUser();
+		closeMynimal();
 		
 	}
 	
 	/**
-	 * Initialization routine
+	 * Initializethe RPG
 	 */
-	public static void startRPG(){
-		
+	public static void openMynimal(){
+		try{
+			// read user file
+			String userdata;
+			FileReader fstream = new FileReader("data/users.dat");
+			BufferedReader userfile = new BufferedReader(fstream);
+			
+			// build username data structures
+			while((userdata = userfile.readLine()) != null){
+				String[] tokens = userdata.split(" ");
+				String username = tokens[0];
+				String password = tokens[1];
+				
+				playerSet.add(username);
+				passwordMap.put(username, password);
+			}
+			
+			// close streams
+			fstream.close();
+			userfile.close();
+		}
+		catch(Exception e){
+			System.err.println("Users file not found.");
+		}
+	}
+	
+	/**
+	 * Close the RPG
+	 */
+	public static void closeMynimal(){
+		try{
+			// store the user data
+			FileWriter fstream = new FileWriter("data/users.dat");
+			BufferedWriter userfile = new BufferedWriter(fstream);
+			
+			// store the user data in file
+			for(String username : passwordMap.keySet()){
+				String password = passwordMap.get(username);
+				userfile.write(username + "\t" + password + "\n");
+			}
+			
+			// close streams
+			fstream.close();
+			userfile.close();
+		}
+		catch(Exception e){
+			System.err.println("Users file not found.");
+		}
 	}
 	
 	/**
 	 * Creates a new Mynimal RPG user
 	 */
 	public static void createUser(){
-		// read a username
-		String username = "";
+		// read a username and password
+		String username = "", password = "";
 		Scanner input = new Scanner(System.in);
 		
 		// poll valid username
 		System.out.println("Please choose a username: ");
-		username = input.nextLine().split(" ")[0];
+		username = input.nextLine().split("\t")[0];
 		while(playerSet.contains(username)){
 			System.out.println("Username taken! Please choose another username: ");
+			username = input.nextLine().split(" ")[0];
+		}
+		
+		// poll valid password
+		System.out.println("Please choose a password: ");
+		password = input.nextLine();
+		while(password.split(" ").length > 1){
+			System.out.println("Password must be one word! Please choose another password: ");
 			username = input.nextLine().split(" ")[0];
 		}
 		
 		// add new user
 		System.out.println("Welcome to MynimalRPG!");
 		playerSet.add(username);
+		input.close();
 	}
 
 }
